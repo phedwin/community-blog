de3ac1afc6ff2ff0e2bf51905f7f5e42ed2cb204
phedwin <engineer@realmelevators.com>
2024-01-23 23:22:16 +0300

    HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -13,7 +13,8 @@ class HandleInertiaRequests extends Middleware
      * @see https://inertiajs.com/server-side-setup#root-template
      * @var string
      */
-    protected $rootView = 'app';
+
+    protected $rootView = 'master';
 
     /**
      * Determines the current asset version.
